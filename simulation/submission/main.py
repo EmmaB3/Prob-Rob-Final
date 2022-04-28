619e68ce79a63b9bc101aa54bc6e33cab6b83df1
EmmaB3 <emma.l.bethel@gmail.com>
2022-04-28 19:07:56 -0400

    done

--- simulation/submission/main.py
+++ simulation/submission/main.py
@@ -16,6 +16,7 @@ def main():
     r.update_pos()
     counter = 0
     reached_goal = False
+    
     while not reached_goal:
         counter = counter + 1
         #create initial image
@@ -27,19 +28,19 @@ def main():
         #cv circle to represent flock
         flock_pos = r.get_flock_pos().astype(int)
         r_flock_pos = r.get_flock_radius()
-        print("flock pos", flock_pos, "flock radius", r_flock_pos)
         robot_pos = r.get_pos().astype(int)
         cv2.circle(img, (flock_pos[0], flock_pos[1]), r_flock_pos, (0,0,255), -1)
-        #cv circle to represent dog
 
-        #cv cr
-        print("robot pos", robot_pos, "robot radius", ROBOT_RADIUS)
+        #cv circle to represent robot
         cv2.circle(img, robot_pos, ROBOT_RADIUS, (155,0,155), -1)
         cv2.waitKey(100)
         cv2.imshow('image', img)
+
+        # move robot and flock
         f.move(robot_pos, ROBOT_RADIUS)
         r.update_pos()
         
+        # check whether goal has been reached
         reached_goal = (np.linalg.norm(f.get_pos() - goal_pos) <= 10)
         print('')
 
